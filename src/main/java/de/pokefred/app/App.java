3c53f914ddd43db47a83bfcd02cfaae6ee5d4f37
PokeFred <Cedric.paelmke@gmx.de>
2024-07-17 15:52:46 +0200

    Update App.java

--- src/main/java/de/pokefred/app/App.java
+++ src/main/java/de/pokefred/app/App.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
     @Override
     public void init() {}
 
     @Override
-    public void start(Stage stage) {}
+    public void start(Stage primaryStage) {}
 
     @Override
     public void stop() {}

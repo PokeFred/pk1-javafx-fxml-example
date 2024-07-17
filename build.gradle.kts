def47eaa13bede0349688d4f6e65b81648840cd4
PokeFred <Cedric.paelmke@gmx.de>
2024-07-17 15:41:40 +0200

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+group = "de.pokefred.app"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "de.pokefred.app.App"
+}
+
+javafx {
+    version = "22.0.1"
+    modules(
+        "javafx.base",
+        "javafx.graphics",
+        "javafx.controls"
+    )
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+plugins {
+    java
+    application
+    id("org.openjfx.javafxplugin") version "0.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+}
---
+++ src/main/java/de/pokefred/app/App.java
@@ -0,0 +1,15 @@
+package de.pokefred.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void init() {}
+
+    @Override
+    public void start(Stage stage) {}
+
+    @Override
+    public void stop() {}
+}
